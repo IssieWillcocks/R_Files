636d574f59cf2483fee39363f86c9db73ce07773
Issie Willcocks <WillcocksIR@cardiff.ac.uk>
2021-01-20 09:13:01 +0000

    Create prepare_sumstats_for_rg.r
    New file for preapring GWAS / CCGWAS sumstat files for rg calculation via ldsc

---
+++ prepare_sumstats_for_rg.r
@@ -0,0 +1,40 @@
+### Preapring Files for Genetic Correlation Analysis Using LDSC ######
+
+### Step One: Read in File ######
+
+sumstats <- read.delim("[Enter File Path Here]", header = TRUE)
+
+### Step Two: Check Header Names ######
+
+col.names(sumstats)
+
+# Taken from https://github.com/bulik/ldsc/wiki/Summary-Statistics-File-Format
+# Required Columns, and best names 
+# SNP -- SNP identifier (e.g., rs number)
+# N -- sample size (which may vary from SNP to SNP).
+# Beta -- z-score. Sign with respect to A1 (warning, possible gotcha)
+# A1 -- first allele (effect allele)
+# A2-- second allele (other allele)
+# P -- P Value 
+
+### Step Three: Drop Unnecessary Columns ######
+
+sumstats <- sumstats[-c([Enter Columns to Drop Here])]
+
+### Step Four: Rename Columns ######
+
+library(tidyverse)
+
+sumstats <- sumstats %>%
+    rename(
+        SNP = [Old Name]
+        N = [Old Name]
+        Beta = [Old Name]
+        A1 = [Old Name]
+        A2 = [Old Name]
+        P = [Old Name]
+    )
+
+### Step Five: Output File as .txt file ######
+
+write.table(sumstats, "[File Name].txt", col.names = TRUE, row.names = FALSE, sep = " ")
